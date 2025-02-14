BlackReloaded/wsl2-ssh-pageant

commit 937e3d8e82ae71a73f6ec6a390e00e91776efa1a

hide ui window on wsl from windows store
Signed-off-by: Tobias Kohlbau <tobias@kohlbau.de>

Makefile
@@ -1,6 +1,6 @@
 
 build:
-	GOOS=windows go build -o wsl2-ssh-pageant.exe main.go
+	GOOS=windows go build -o wsl2-ssh-pageant.exe -ldflags -H=windowsgui main.go
 
 install: build
 	mv wsl2-ssh-pageant.exe ~/.ssh/

octivi/wsl2-ssh-bridge

commit 73f93be03d6f9c0e9419180f6e74ebe85905c88d

fix(main): fix staled binary fix_staled_instances
New wsl2-ssh-bridge is created for every GPG connection, but it is not
removed after the connection is closed.

After investigation in https://github.com/BlackReloaded/wsl2-ssh-pageant/issues/38
looks like issue is related to change introduced in
https://github.com/BlackReloaded/wsl2-ssh-pageant/commit/937e3d8e82ae71a73f6ec6a390e00e91776efa1a
as a fix to https://github.com/BlackReloaded/wsl2-ssh-pageant/issues/30

This commit reverts the change and fixes the issue.

.github/workflows/go.yml
@@ -17,7 +17,7 @@ jobs:
       - name: Build
         env:
           GOOS: windows
-        run: go build -o wsl2-ssh-bridge.exe -ldflags -H=windowsgui main.go
+        run: go build -o wsl2-ssh-bridge.exe main.go
 
       - name: Upload artifact
         uses: actions/upload-artifact@v1.0.0

Makefile
@@ -2,7 +2,7 @@ all: listen
 
 build:
 	@echo "Building wsl2-ssh-bridge..."
-	@GOOS=windows go build -o wsl2-ssh-bridge.exe -ldflags -H=windowsgui main.go
+	@GOOS=windows go build -o wsl2-ssh-bridge.exe main.go
 
 SSH_PATH := "${USERPROFILE}/.ssh/"
 install: build